fdd9be2d0789b570113d56f010ca619646d8b1a1
mancinoa <alex.mancino@gmail.com>
2019-11-05 13:03:38 -0500

    Adding a background
    The background, scrolling clouds, and music now offer a very relaxing experience.

--- resources/js/animate.js
+++ resources/js/animate.js
@@ -1,6 +1,6 @@
 function myMove(){
-    const refreshRate = 1000 / 60;
-    const maxXPosition = 400;
+    const refreshRate = 10000 / 60;
+    const maxXPosition = 880;
     let rect = document.getElementById('animate');
     let speedX = 1;
     let positionX = 0;
@@ -13,30 +13,17 @@ function myMove(){
     }, refreshRate);
 }
 function myMove2(){
-    const refreshRate = 2000 / 60;
-    const maxXPosition = 400;
+    const refreshRate = 20000 / 60;
+    const maxXPosition = 880;
     let rect = document.getElementById('animate2');
     let speedX = 1;
-    let positionX = 0;
+    let positionX = 1;
     window.setInterval(() => {
     positionX = positionX + speedX;
         if (positionX > maxXPosition || positionX < 0) {
         speedX = speedX * (-1);
         }
     rect.style.left = positionX + 'px';
-    }, refreshRate);
-}
-function myMove3(){
-    const refreshRate = 500 / 60;
-    const maxXPosition = 400;
-    let rect = document.getElementById('animate');
-    let speedX = 1;
-    let positionX = 0;
-    window.setInterval(() => {
-    positionX = positionX + speedX;
-        if (positionX > maxXPosition || positionX < 0) {
-        speedX = speedX * (-1);
-        }
-    rect.style.left = positionX + 'px';
+    rect.style.top = positionX/20 + 'px';
     }, refreshRate);
 }
